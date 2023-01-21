abe9973ed41d11a3e452be1ecab24e849e5eac55
San-kalp <sankalp.chordia20@vit.edu>
2023-01-21 13:49:12 +0100

    nft

---
+++ scripts/deploy.js
@@ -0,0 +1,22 @@
+
+const hre = require("hardhat");
+
+async function main() {
+
+
+  const NFTMarketplace = await hre.ethers.getContractFactory("NFTMarketplace");
+  const nftMarketplace = await NFTMarketplace.deploy();
+
+  await nftMarketplace.deployed();
+
+  console.log(
+    `deployed contract Address ${nftMarketplace.address}`
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
